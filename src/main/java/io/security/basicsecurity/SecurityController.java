a7b0e14b5e58f144f8a542df3c9a44d92494e97e
makadamia0055 <makend91@naver.com>
2023-10-23 18:12:59 +0900

    chapter 3, section 5

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,15 +1,43 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpSession session){
+        // 기본적인 방법으로 꺼내오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 세션에서 꺼내오기
+        SecurityContext context = (SecurityContext)session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
         return "home";
     }
+
+    @GetMapping("/thread")
+    public String thread(){
+        new Thread(
+
+                new Runnable(){
+                    @Override
+                    public void run(){
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+        ).start();
+    return "thread";
+
+    }
+
+
+
 
     @GetMapping("/loginPage")
     public String loginPage(){
